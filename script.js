d2414dfb7daee50f104c7a48ca431f40f173f1df
Alae <aamimi454@gmail.com>
2025-10-30 11:23:35 +0000

    first sec display in js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var userevent;
+document.getElementById("secsec").style.display = "none";
+function aftereserve(index){
+     userevent = document.getElementById(index);
+    document.getElementById('allsec').style.display = "none";
+    document.getElementById("secsec").style.display = "flex";
+}
+console.log(userevent);
+var secondpage = document.getElementById('dots');
+secondpage.innerText.userevent(index);
